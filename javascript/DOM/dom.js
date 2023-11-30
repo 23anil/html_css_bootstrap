e21d5ded0617c46ba8bf9223f6de60b64fe3dd57
anil <anillokikere@gmail.com>
2023-11-30 19:11:13 +0530

    update

---
+++ javascript/DOM/dom.js
@@ -0,0 +1,7 @@
+let element = document.getElementsByClassName("test");
+console.log("element", element);
+
+// element.innnerHTML = "<button>click me</button>";
+
+element.style.color = "white";
+element.style.backgroundColor = "#8a2be2";
